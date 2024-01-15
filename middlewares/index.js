3cf03acf8713da48ac4c3f0086655bf6f392c3d9
kenmuraki5 <89627339+Kenmuraki5@users.noreply.github.com>
2024-01-15 08:56:05 +0700

    middlewares

---
+++ middlewares/index.js
@@ -0,0 +1,21 @@
+const { admin } = require("../Firebase/FirebaseConfig");
+
+const isLoggedIn = (req, res, next) => {
+    const token = req.header.authorization;
+
+    if(token){
+        return res.status(401).json("unauthorize");
+    }
+
+    admin.auth().verifyIdToken(token)
+        .then(decodetoken => {
+            req.user = decodetoken;
+            next();
+        })
+        .catch(err => {
+            console.log("verifying token error");
+            res.status(401).json("error");
+        })
+
+
+}
