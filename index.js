cc62c3fddf5f5ba9e6d7fe0cbfa4298f195574a3
RomeyKung <88416990+RomeyKung@users.noreply.github.com>
2024-03-25 18:50:20 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,61 +1,46 @@
 const express = require('express');
 const { getStorage, getDownloadURL } = require('firebase-admin/storage');
-const cors = require("cors");
+const cors = require('cors');
 
 const app = express();
 const port = 3000; // Set your desired port number
 
 const { db } = require('./Firebase/FirebaseConfig.js');
-const {bucket} = require('./Firebase/FirebaseConfig.js');
+const { bucket } = require('./Firebase/FirebaseConfig.js');
 
 app.use(cors());
 app.use(express.json());
 
+// Define Permissions-Policy headers
+app.use((req, res, next) => {
+  res.setHeader('Permissions-Policy', 'interest-cohort=()'); // Adjust or remove as necessary
+  next();
+});
 
-
-
-//router
+// Define routes (assuming these are your API endpoints)
 const authenticationRouter = require('./routes/user/Authentication.js');
-const subject = require("./routes/subject/subject.js")
-const question = require("./routes/subject/question.js")
-const answer = require("./routes/subject/answer.js")
-const post = require("./routes/post/post.js")
-const review = require("./routes/subject/Review.js")
-const studyPlan = require("./routes/subject/studyPlan.js")
-app.use("/api", authenticationRouter.router);
-app.use("/api", subject.router)
-app.use("/api", question.router)
-app.use("/api", answer.router)
-app.use("/api", post.router)
-app.use("/api", review.router)
-app.use("/api", studyPlan.router)
-//
+const subject = require('./routes/subject/subject.js');
+const question = require('./routes/subject/question.js');
+const answer = require('./routes/subject/answer.js');
+const post = require('./routes/post/post.js');
+const review = require('./routes/subject/Review.js');
+const studyPlan = require('./routes/subject/studyPlan.js');
 
+app.use('/api', authenticationRouter.router);
+app.use('/api', subject.router);
+app.use('/api', question.router);
+app.use('/api', answer.router);
+app.use('/api', post.router);
+app.use('/api', review.router);
+app.use('/api', studyPlan.router);
 
+// Define a simple route for testing
 app.get('/', (req, res) => {
-    res.send('Hello, this is your backend!');
-  });
-
-// app.post('/api/create', async (req, res) => {
-//   // const { name, email, password } = req.body;
-//   const user = { name:"rome" };
-//   const userRef = await db.collection('users').add(user);
-//   const userDoc = await userRef.get();
-//   res.send(userDoc.data());
-
-// });
-
-// app.get('/api/img', async (req, res) => {
-//   const fileRef = bucket.file('Python-Imports_Watermarked.ae72c8a00197.jpg');
-//   const downloadURL= await getDownloadURL(fileRef);
-//   console.log(downloadURL);
-//   res.send(downloadURL);
-// });
-
-
+  res.send('Hello, this is your backend!');
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-  
+
 module.exports = app;
