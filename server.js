0e3de7c675cd9f13a9c08b4007a29cc7724ec279
RomeyKung <88416990+RomeyKung@users.noreply.github.com>
2024-03-25 17:35:10 +0700

    fix setHeader

--- server.js
+++ server.js
@@ -11,6 +11,32 @@ const {bucket} = require('./Firebase/FirebaseConfig.js');
 app.use(cors());
 app.use(express.json());
 
+
+const whitelist = [
+  '*'
+];
+
+app.use((req, res, next) => {
+  const origin = req.get('referer');
+  const isWhitelisted = whitelist.find((w) => origin && origin.includes(w));
+  if (isWhitelisted) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+  }
+  // Pass to next layer of middleware
+  if (req.method === 'OPTIONS') res.sendStatus(200);
+  else next();
+});
+
+const setContext = (req, res, next) => {
+  if (!req.context) req.context = {};
+  next();
+};
+app.use(setContext);
+
+
 //router
 const authenticationRouter = require('./routes/user/Authentication.js');
 const subject = require("./routes/subject/subject.js")
